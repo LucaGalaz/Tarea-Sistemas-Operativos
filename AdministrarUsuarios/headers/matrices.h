8569f52ddc34d038e359de3c4b784ba79d087dc7
mvrcelolara <138083554+mvrcelolara@users.noreply.github.com>
2025-09-07 16:41:43 -0300

    Update matrices.h

--- AdministrarUsuarios/headers/matrices.h
+++ AdministrarUsuarios/headers/matrices.h
@@ -1,12 +1,15 @@
 #ifndef MATRICES_H
 #define MATRICES_H
 
-#include <iostream>
-using namespace std;
+#include <vector>
+#include <string>
 
-inline void multiplicarMatrices() {
-    cout << "[Matrices] Multiplicacion de matrices NxN (en construccion)\n";
-    // Aquí luego deberías invocar el programa externo que multiplica matrices
-}
+typedef std::vector<std::vector<double>> Matriz;
+
+// Declaración de funciones
+Matriz leerMatriz(const std::string& ruta, char sep);
+Matriz multiplicar(const Matriz& A, const Matriz& B);
+void imprimirMatriz(const Matriz& M);
+void ejecutarMultiplicacion();
 
 #endif
