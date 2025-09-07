988ea85bd4f891ea97e62e0b91db775b8248d916
1lucaglaz <141769186+LucaGalaz@users.noreply.github.com>
2025-09-07 15:45:22 -0300

    Add files via upload

---
+++ AdministrarUsuarios/headers/juego.h
@@ -0,0 +1,11 @@
+#ifndef JUEGO_H
+#define JUEGO_H
+
+#include <iostream>
+using namespace std;
+
+inline void iniciarJuego() {
+    cout << "[Juego] Módulo en construccion\n";
+}
+
+#endif
---
+++ AdministrarUsuarios/headers/matrices.h
@@ -0,0 +1,12 @@
+#ifndef MATRICES_H
+#define MATRICES_H
+
+#include <iostream>
+using namespace std;
+
+inline void multiplicarMatrices() {
+    cout << "[Matrices] Multiplicacion de matrices NxN (en construccion)\n";
+    // Aquí luego deberías invocar el programa externo que multiplica matrices
+}
+
+#endif
